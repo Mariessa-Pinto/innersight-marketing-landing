dddb1c732e894aeee0edad8fdd0f173c1a4dd4f2
pjiang604 <patriciaejiang@gmail.com>
2023-11-15 14:12:23 -0800

    begin testimonial section

--- components/benefits/BenefitBlob.js
+++ components/benefits/BenefitBlob.js
@@ -6,16 +6,26 @@ import Image from 'next/image';
 export default function BenefitBlob({
     header,
     imgSrc,
-    description
+    description,
+    key
 }) {
     return (
         <>
             <main className={styles.main}>
-                <Image src='/benefitBlob.png'  width={383}
-                        height={553} />
+                {
+                    header === "Discover the Benefit\nof AI Insights" ?
+                        <Image src='/benefitBlob_flip.png'
+                            width={383}
+                            height={553} />
+                        :
+                        <Image src='/benefitBlob.png'
+                            width={383}
+                            height={553} />
+                }
+
                 <div className={styles.content}>
                     <h3>{header}</h3>
-                    <Image src={imgSrc} alt="image" className={styles.benefitImg} width={50} height={50} />
+                    <Image src={imgSrc} alt="image" className={styles.benefitImg} width={150} height={150} />
                     <p>{description}</p>
 
                 </div>
